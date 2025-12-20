a824671a72250edf4059642c92b0aceced701342
AshBoomstick1 <ashcjohnson56@proton.me>
2025-12-20 14:53:06 -0500

    made vaibale static stripes

--- modules/stripes.js
+++ modules/stripes.js
@@ -3,13 +3,21 @@ const body = document.getElementsByTagName("body")[0];
 
 export const stripes = () => {
     let svg = document.getElementById("hero_svg")
-    svg.style.position = "absolute;";
-    svg.style.left = "0px;";
-    svg.style.top = "0px";
+    const width = body.offsetWidth;
+    const height = hero_area.offsetHeight + 140;
+    const size = 175;
+
+    svg.setAttribute("style", "top: 0px; left: 0px;");
     
-    svg.setAttribute("width", `${body.offsetWidth}px`);
-    svg.setAttribute("height", `${body.offsetHeight}px`);
+    svg.setAttribute("width", `${width}px`);
+    svg.setAttribute("height", `${height}`);
     svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
 
     svg.style.backgroundColor = "red";
+
+    const stripe_limit = Math.sqrt((width * width) + (height * height))
+    for (let i = -stripe_limit - size; i < stripe_limit + size; i += size) {
+        svg.innerHTML += `<polygon points=\"${width + i},${-(size / 2) + i} ${width + i},${(size / 2) + i} ${i},${height + (size / 2) + i} ${-(size / 2) + i},${height + i}\" fill="#4cff79ff" />`;
+    }
+
 }
