4d6899acc98b65f01efed56491b44a2239b59ff6
AshBoomstick1 <143759985+AshBoomstick1@users.noreply.github.com>
2026-01-11 18:20:49 -0500

    sorting works

--- modules/sorting.js
+++ modules/sorting.js
@@ -7,8 +7,9 @@ let svg = document.getElementById("hero_svg");
 const width = body.offsetWidth;
 const height = hero_area.offsetHeight; //140
 
-const setUpData = (x, y, width, height) => {
-  const totalDataCount = Math.floor(width / 5) + 1;
+const setUpData = (x, y, width, height, direction) => {
+  const colWidth = 10;
+  const totalDataCount = Math.floor(width / colWidth) + 1;
   let numList = []
   let randNumList = []
   let firstIdx;
@@ -23,10 +24,21 @@ const setUpData = (x, y, width, height) => {
 
   for (let i = 0; i < totalDataCount; i++) {
     const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-    rect.setAttribute('x', `${i * 5 + x}`);
-    rect.setAttribute('y', `${randNumList[i] * height/totalDataCount + y - height/totalDataCount}`);
-    rect.setAttribute('width', `${4}`);
-    rect.setAttribute('height', `${height - randNumList[i] * height/totalDataCount + height/totalDataCount}`);
+    switch (direction) {
+      case "up":
+        rect.setAttribute('x', `${i * colWidth + x}`);
+        rect.setAttribute('y', `${randNumList[i] * height/totalDataCount + y - height/totalDataCount}`);
+        rect.setAttribute('width', `${colWidth - 1}`);
+        rect.setAttribute('height', `${height - randNumList[i] * height/totalDataCount + height/totalDataCount}`);
+        break;
+      case "down":
+        rect.setAttribute('x', `${i * colWidth + x}`);
+        rect.setAttribute('y', `${y - height/totalDataCount}`);
+        rect.setAttribute('width', `${colWidth - 1}`);
+        rect.setAttribute('height', `${height - randNumList[i] * height/totalDataCount}`);
+        break;
+    }
+    
     rect.setAttribute('fill', "rgb(220, 187, 0)");
     svg.appendChild(rect);
     if (i==0) firstIdx = Array.prototype.indexOf.call(svg.children, rect);
@@ -41,9 +53,21 @@ export const sort = () => {
   svg.setAttribute("width", `${width}px`);
   svg.setAttribute("height", `${height}px`);
   svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-  const dataSet1 = setUpData(0, 100, 100, 100);
-  const dataSet2 = setUpData(100, 200, 100, 100);
+  const dataSet1 = setUpData(0, 150, 500, 350, "up");
+  const dataSet2 = setUpData(0, 500, 500, 350, "down");
+  const dataSet5 = setUpData(500, 150, 400, 350, "up");
+  const dataSet6 = setUpData(500, 500, 400, 350, "down");
+  const dataSet3 = setUpData(900, 150, 400, 350, "up");
+  const dataSet4 = setUpData(900, 500, 400, 350, "down");
+  const dataSet7 = setUpData(1300, 150, 400, 350, "up");
+  const dataSet8 = setUpData(1300, 500, 400, 350, "down");
 
   bubble(dataSet1);
   bubble(dataSet2);
+  bubble(dataSet3);
+  bubble(dataSet4);
+  bubble(dataSet6);
+  bubble(dataSet5);
+  bubble(dataSet7);
+  bubble(dataSet8);
 }
--- modules/sortingAlgs/bubble.js
+++ modules/sortingAlgs/bubble.js
@@ -10,19 +10,47 @@ const isSorted = (dataSetId) => {
   return true;
 }
 
+const moveAnimation = (dataIdx, xChange, startNextSwap, i, endI, dataSetId) => {
+  const move = document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect")[dataIdx].animate([
+      {transform: `translate3d(0px, 0px, 0px)`},
+      {transform: `translate3d(${xChange}px, 0px, 0px)`},
+    ],
+    {
+      duration: 0,
+      easing: "ease-in-out",
+      iterations: 1
+    },
+  );
+  
+  move.addEventListener("finish", () => {
+    console.log("DONE")
+    document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect")[dataIdx].setAttribute("x", Number(document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect")[dataIdx].getAttribute("x")) + xChange)
+    if (startNextSwap == true) {
+      svg.insertBefore(document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect")[dataIdx], document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect")[dataIdx - 1]);
+      swap(i + 1, endI, dataSetId);
+    }
+  });
+}
+
+const swap = (i, endI, dataSetId) => {
+  if (i < endI) {
+    console.log(Number(document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect")[dataSetId[0] + i].getAttribute("height")), Number(document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect")[dataSetId[0] + i + 1].getAttribute("height")), Number(document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect")[dataSetId[0] + i].getAttribute("height")) > Number(document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect")[dataSetId[0] + i + 1].getAttribute("height")))
+    if (Number(document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect")[dataSetId[0] + i].getAttribute("height")) > Number(document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect")[dataSetId[0] + i + 1].getAttribute("height"))) {
+      //document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect")[dataSetId[0] + i].setAttribute("x", Number(document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect")[dataSetId[0] + i].getAttribute("x")) - 5)
+      //document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect")[dataSetId[0] + i + 1].setAttribute("x", Number(document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect")[dataSetId[0] + i + 1].getAttribute("x")) + 5)
+      moveAnimation(dataSetId[0] + i, Number(document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect")[dataSetId[0] + i].getAttribute("width")) + 1, false, i, endI, dataSetId);
+      moveAnimation(dataSetId[0] + i + 1, (Number(document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect")[dataSetId[0] + i + 1].getAttribute("width")) + 1) * -1, true, i, endI, dataSetId);
+    } else {
+      swap(i + 1, endI, dataSetId)
+    }
+  } else if (!isSorted(dataSetId)) {
+    swap(0, endI, dataSetId);
+  }
+}
+
 export const bubble = (dataSetId) => {
   //console.log(!isSorted(dataSetId))
   const data = [].slice.apply(document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect"), dataSetId);
   console.log([].slice.apply(document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect"), dataSetId), [].slice.apply(document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect"), dataSetId).length, document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect").length, dataSetId)
-  while (!isSorted(dataSetId)) {
-    //console.log("SOERTING", data.length)
-    for (let i = 0; i < data.length - 1; i++) {
-      console.log(document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect")[dataSetId[0] + i], document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect")[dataSetId[0] + i + 1])
-      if (Number(document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect")[dataSetId[0] + i].getAttribute("height")) > Number(document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect")[dataSetId[0] + i + 1].getAttribute("height"))) {
-        svg.insertBefore(document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect")[dataSetId[0] + i + 1], document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect")[dataSetId[0] + i]);
-        document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect")[dataSetId[0] + i].setAttribute("x", Number(document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect")[dataSetId[0] + i].getAttribute("x")) - 5)
-        document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect")[dataSetId[0] + i + 1].setAttribute("x", Number(document.getElementsByTagNameNS('http://www.w3.org/2000/svg', "rect")[dataSetId[0] + i + 1].getAttribute("x")) + 5)
-      }
-    }
-  }
+  swap(0, data.length - 1, dataSetId);
 }
